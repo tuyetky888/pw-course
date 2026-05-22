7af180ee29c7a187673be5f873416d3eac6becc4
Rose Nguyen <tuyetky888@gmail.com>
2026-05-22 14:26:05 +0700

    bai tap buoi 1

---
+++ tests/example.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from "@playwright/test";
+
+test("Tài liệu học automation test", async ({ page }) => {
+  await page.goto("https://material.playwrightvn.com/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test("Bài học 1: Register Page link", async ({ page }) => {
+  await page.goto("https://material.playwrightvn.com/");
+
+  // Click the get started link.
+  await page.getByRole("link", { name: "Bài học 1: Register Page" }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(
+    page.getByRole("heading", { name: "User Registration" }),
+  ).toBeVisible();
+});
