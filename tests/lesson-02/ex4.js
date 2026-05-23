1c70b674da2a230311b2b913638e6289ad3bf12c
Rose Nguyen <tuyetky888@gmail.com>
2026-05-23 23:05:52 +0700

    bai tap buoi 2 ex4.js

---
+++ tests/lesson-02/ex4.js
@@ -0,0 +1,9 @@
+const myHeight = 160;
+
+if (myHeight>100){
+canNangLyTuong = myHeight*9/10;
+mucCanToiDa = myHeight-100;
+mucCanToiThieu = (myHeight-100)*8/10;
+console.log(canNangLyTuong,mucCanToiDa,mucCanToiThieu);
+}
+
