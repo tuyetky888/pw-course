df77c1ee339ce0c9cc0243a8831a6f1318104e02
Rose Nguyen <tuyetky888@gmail.com>
2026-05-23 22:58:13 +0700

    bai tap buoi 2 ex3.js

---
+++ tests/lesson-02/ex3.js
@@ -0,0 +1,12 @@
+const number = 8;
+
+if (number > 0){
+    console.log ("Giá trị bạn nhập là số dương");
+}else if (number < 0){
+    console.log ("Giá trị bạn nhập là số âm");
+}else{
+    console.log ("Giá trị bạn nhập là số 0");
+};
+
+
+
